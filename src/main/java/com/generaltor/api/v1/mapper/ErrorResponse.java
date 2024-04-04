75066ce4fbbb79b38745670f8006297ca62fbe03
Gaëtan L. H.-F <81755261+gaetanlhf@users.noreply.github.com>
2024-04-04 14:00:28 +0200

    Create ErrorResponse.java

---
+++ src/main/java/com/generaltor/api/v1/mapper/ErrorResponse.java
@@ -0,0 +1,43 @@
+package com.generaltor.api.v1.mapper;
+
+public class ErrorResponse {
+    private Error error;
+
+    public ErrorResponse(int code, String message) {
+        this.error = new Error(code, message);
+    }
+
+    public Error getError() {
+        return error;
+    }
+
+    public void setError(Error error) {
+        this.error = error;
+    }
+
+    public static class Error {
+        private int code;
+        private String message;
+
+        public Error(int code, String message) {
+            this.code = code;
+            this.message = message;
+        }
+
+        public int getCode() {
+            return code;
+        }
+
+        public void setCode(int code) {
+            this.code = code;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
+        public void setMessage(String message) {
+            this.message = message;
+        }
+    }
+}
