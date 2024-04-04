af62395a67b6c2d4362a332f296a2e84b8750954
Gaëtan L. H.-F <81755261+gaetanlhf@users.noreply.github.com>
2024-04-04 14:00:06 +0200

    Create License.java

---
+++ src/main/java/com/generaltor/api/v1/entity/License.java
@@ -0,0 +1,30 @@
+package com.generaltor.api.v1.entity;
+
+public class License {
+    private String licenseKey;
+    private boolean isDisabled;
+
+    public License() {
+    }
+
+    public License(String licenseKey, boolean isDisabled) {
+        this.licenseKey = licenseKey;
+        this.isDisabled = isDisabled;
+    }
+
+    public String getLicenseKey() {
+        return licenseKey;
+    }
+
+    public void setLicenseKey(String licenceKey) {
+        this.licenseKey = licenceKey;
+    }
+
+    public boolean isDisabled() {
+        return isDisabled;
+    }
+
+    public void setDisabled(boolean disabled) {
+        isDisabled = disabled;
+    }
+}
