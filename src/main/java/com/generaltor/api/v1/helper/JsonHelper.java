9c6a0e45b7435cbd976597422589bdffea7828ff
Gaëtan L. H.-F <81755261+gaetanlhf@users.noreply.github.com>
2024-04-04 14:00:24 +0200

    Create JsonHelper.java

---
+++ src/main/java/com/generaltor/api/v1/helper/JsonHelper.java
@@ -0,0 +1,22 @@
+package com.generaltor.api.v1.helper;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.jboss.logging.Logger;
+
+public class JsonHelper {
+    private static final Logger LOG = Logger.getLogger(JsonHelper.class);
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    private JsonHelper() {
+    }
+
+    public static String serializeToJson(Object object) {
+        try {
+            return objectMapper.writeValueAsString(object);
+        } catch (JsonProcessingException e) {
+            LOG.error("Error serializing object to JSON", e);
+            return "{\"error\":{\"code\":500, \"message\":\"Serialization error\"}}";
+        }
+    }
+}
