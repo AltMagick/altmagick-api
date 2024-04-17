6018966e8cd8a4b0a626537dd157eb3b3cceea9e
Gaëtan L. H.-F. <81755261+gaetanlhf@users.noreply.github.com>
2024-04-17 17:35:40 +0200

    Create SubHelper.java

---
+++ src/main/java/com/generaltor/api/v1/helper/SubHelper.java
@@ -0,0 +1,28 @@
+package com.generaltor.api.v1.helper;
+
+import com.generaltor.api.v1.entity.Sub;
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.DocumentSnapshot;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.QueryDocumentSnapshot;
+import com.google.cloud.firestore.QuerySnapshot;
+
+public class SubHelper {
+
+    private final Firestore firestore;
+
+    public SubHelper(Firestore firestore) {
+        this.firestore = firestore;
+    }
+
+    public DocumentSnapshot getSubByLicenseKey(String licenseKey) throws Exception {
+        ApiFuture<QuerySnapshot> query = firestore.collection("subs").whereEqualTo("license.licenseKey", licenseKey).get();
+        QuerySnapshot querySnapshot = query.get();
+
+        if (querySnapshot.isEmpty()) {
+            throw new Exception("License not found");
+        }
+
+        return querySnapshot.getDocuments().getFirst();
+    }
+}
