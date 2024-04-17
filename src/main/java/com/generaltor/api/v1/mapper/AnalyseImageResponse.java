ad6163072614ac72f61cdb1705be205c74a90ca6
Gaëtan L. H.-F. <81755261+gaetanlhf@users.noreply.github.com>
2024-04-17 17:36:18 +0200

    Create AnalyseImageResponse.java

---
+++ src/main/java/com/generaltor/api/v1/mapper/AnalyseImageResponse.java
@@ -0,0 +1,84 @@
+package com.generaltor.api.v1.mapper;
+
+public class AnalyseImageResponse {
+
+    private Data data;
+
+    public AnalyseImageResponse(String subId, String analyseText, long elapsedTime, long timestamp, int usageCount, String language) {
+        this.data = new Data(subId, analyseText, elapsedTime, timestamp, usageCount, language);
+    }
+
+    public Data getData() {
+        return data;
+    }
+
+    public void setData(Data data) {
+        this.data = data;
+    }
+
+    public static class Data {
+        private String subId;
+        private String analyseText;
+        private long elapsedTime;
+        private long timestamp;
+        private int usageCount;
+        private String language; // Ajout du nouveau champ
+
+        public Data(String subId, String analyseText, long elapsedTime, long timestamp, int usageCount, String language) {
+            this.subId = subId;
+            this.analyseText = analyseText;
+            this.elapsedTime = elapsedTime;
+            this.timestamp = timestamp;
+            this.usageCount = usageCount;
+            this.language = language; // Initialisation du nouveau champ
+        }
+
+        public String getSubId() {
+            return subId;
+        }
+
+        public void setSubId(String subId) {
+            this.subId = subId;
+        }
+
+        public String getAnalyseText() {
+            return analyseText;
+        }
+
+        public void setAnalyseText(String analyseText) {
+            this.analyseText = analyseText;
+        }
+
+        public long getElapsedTime() {
+            return elapsedTime;
+        }
+
+        public void setElapsedTime(long elapsedTime) {
+            this.elapsedTime = elapsedTime;
+        }
+
+        public long getTimestamp() {
+            return timestamp;
+        }
+
+        public void setTimestamp(long timestamp) {
+            this.timestamp = timestamp;
+        }
+
+        public int getUsageCount() {
+            return usageCount;
+        }
+
+        public void setUsageCount(int usageCount) {
+            this.usageCount = usageCount;
+        }
+
+        public String getLanguage() {
+            return language;
+        }
+
+        public void setLanguage(String language) {
+            this.language = language;
+        }
+    }
+}
