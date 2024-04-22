e6848d99f986dd48b789eaf7945293d5da8dec1c
Gaëtan L. H.-F. <81755261+gaetanlhf@users.noreply.github.com>
2024-04-22 18:18:56 +0200

    Update VertexImage.java

--- src/main/java/com/generaltor/api/v1/helper/VertexImage.java
+++ src/main/java/com/generaltor/api/v1/helper/VertexImage.java
@@ -1,44 +1,61 @@
 package com.generaltor.api.v1.helper;
 
+import com.generaltor.api.v1.entity.ImageRequest;
 import com.google.cloud.vertexai.VertexAI;
 import com.google.cloud.vertexai.api.GenerateContentResponse;
 import com.google.cloud.vertexai.generativeai.ContentMaker;
 import com.google.cloud.vertexai.generativeai.GenerativeModel;
 import com.google.cloud.vertexai.generativeai.PartMaker;
 import com.google.cloud.vertexai.generativeai.ResponseHandler;
-import org.apache.commons.io.IOUtils;
+import org.apache.tika.Tika;
 
-import java.io.BufferedInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.net.URLConnection;
+import java.util.Arrays;
+import java.util.List;
 
 public class VertexImage {
 
-    public static String analyseImage(String projectId, String location, String modelName, String prompt, InputStream image, String language) throws IOException {
+    public static String analyseImage(String projectId, String location, String modelName, String prompt, ImageRequest imageRequest, String language) throws IOException {
         try (VertexAI vertexAi = new VertexAI(projectId, location)) {
             GenerativeModel model = new GenerativeModel(modelName, vertexAi);
 
-            if (!image.markSupported()) {
-                image = new BufferedInputStream(image);
+            URL url = new URL(imageRequest.getImageUrl());
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestProperty("User-Agent", "Mozilla/5.0 (X11; Linux x86_64; rv:124.0) Gecko/20100101 Firefox/124.0");
+            InputStream urlStream = connection.getInputStream();            PushbackInputStream pushUrlStream = new PushbackInputStream(urlStream, 100);
+            byte[] firstBytes = new byte[100];
+            pushUrlStream.read(firstBytes);
+            pushUrlStream.unread(firstBytes);
+
+            Tika tika = new Tika();
+            ByteArrayInputStream bais = new ByteArrayInputStream(firstBytes);
+            String mimeType = tika.detect(bais);
+            String formatName = mimeType.startsWith("image/") ? mimeType.substring("image/".length()) : null;
+
+            List<String> acceptedFormats = Arrays.asList("jpeg", "png", "webp", "heic", "heif");
+
+            BufferedImage image = ImageIO.read(pushUrlStream);
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+
+            if (!acceptedFormats.contains(formatName)) {
+                ImageIO.write(image, "JPEG", byteArrayOutputStream);
+                formatName = "jpeg";
+            } else {
+                ImageIO.write(image, formatName, byteArrayOutputStream);
             }
 
-            String mimeType = getMimeType(image);
-
-            byte[] imageData = IOUtils.toByteArray(image);
+            byte[] imageData = byteArrayOutputStream.toByteArray();
 
             GenerateContentResponse response = model.generateContent(ContentMaker.fromMultiModalData(
-                    PartMaker.fromMimeTypeAndData(mimeType, imageData), prompt + "\nLanguage: " + language
+                    PartMaker.fromMimeTypeAndData("image/" + formatName.toLowerCase(), imageData), prompt + "\nLanguage: " + language
             ));
 
             return ResponseHandler.getText(response).trim();
         }
-    }
-
-    private static String getMimeType(InputStream image) throws IOException {
-        image.mark(10 * 1024);
-        String mimeType = URLConnection.guessContentTypeFromStream(image);
-        image.reset();
-        return mimeType;
     }
 }
