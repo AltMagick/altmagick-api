05b7be0125a852c8c95f584dd3467452c465cc1d
Gaëtan L. H.-F. <81755261+gaetanlhf@users.noreply.github.com>
2024-04-22 18:06:35 +0200

    Create ImageRequest.java

---
+++ src/main/java/com/generaltor/api/v1/entity/ImageRequest.java
@@ -0,0 +1,13 @@
+package com.generaltor.api.v1.entity;
+
+public class ImageRequest {
+    private String imageUrl;
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
